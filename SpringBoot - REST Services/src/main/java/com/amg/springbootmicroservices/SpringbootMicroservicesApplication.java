1bd8868bb3b5be3d3f4ebd1c29b4e7c516e7114b
kamauge <kamaugerald2@gmail.com>
2023-04-18 08:10:18 +0300

    spring doc for API documentation that uses swagger.io ui

--- SpringBoot - REST Services/src/main/java/com/amg/springbootmicroservices/SpringbootMicroservicesApplication.java
+++ SpringBoot - REST Services/src/main/java/com/amg/springbootmicroservices/SpringbootMicroservicesApplication.java
@@ -1,11 +1,36 @@
 package com.amg.springbootmicroservices;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot REST API documentation",
+				description = "Spring Boot REST API documentation",
+				version = "v1.0",
+
+				contact = @Contact(
+						name = "Gerald Kamau",
+						email = "kamaugerald2@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot User Management documentation"
+
+		)
+)
+
 public class SpringbootMicroservicesApplication {
 	@Bean
 	public ModelMapper modelMapper(){
